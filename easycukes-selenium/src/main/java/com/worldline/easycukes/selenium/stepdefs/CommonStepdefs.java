e5fa1e06973f9ed6e2b0e0a6cfcdb86b88973165
Antoine Neveux <antoine.neveux@gmail.com>
2014-11-24 16:27:29 +0100

    Added license header to all source files

--- easycukes-selenium/src/main/java/com/worldline/easycukes/selenium/stepdefs/CommonStepdefs.java
+++ easycukes-selenium/src/main/java/com/worldline/easycukes/selenium/stepdefs/CommonStepdefs.java
@@ -1,3 +1,20 @@
+/*
+ * EasyCukes is just a framework aiming at making Cucumber even easier than what it already is.
+ * Copyright (C) 2014 Worldline or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
 package com.worldline.easycukes.selenium.stepdefs;
 
 import org.junit.Assert;
