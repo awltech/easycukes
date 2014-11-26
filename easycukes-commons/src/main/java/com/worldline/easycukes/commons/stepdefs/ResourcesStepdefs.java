15c8d922786feed97823fec8d93f921790535c97
Antoine Neveux <antoine.neveux@gmail.com>
2014-11-26 11:17:19 +0100

    Moved ResourcesStepdefs to EasyCukes common

--- easycukes-commons/src/main/java/com/worldline/easycukes/commons/stepdefs/ResourcesStepdefs.java
+++ easycukes-commons/src/main/java/com/worldline/easycukes/commons/stepdefs/ResourcesStepdefs.java
@@ -15,7 +15,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
-package com.worldline.easycukes.scm.stepdefs;
+package com.worldline.easycukes.commons.stepdefs;
 
 import java.io.File;
 import java.io.IOException;
---
+++ easycukes-commons/src/main/java/com/worldline/easycukes/commons/stepdefs/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package aims at containing all Cucumber stepdefs to be used while dealing with common operations.
+ *
+ * @author aneveux
+ *
+ */
+package com.worldline.easycukes.commons.stepdefs;
