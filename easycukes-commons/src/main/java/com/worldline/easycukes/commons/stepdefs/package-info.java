692ab8d7fb58037a03a18171675a42d29d1a5219
Antoine Neveux <antoine.neveux@gmail.com>
2014-12-01 16:49:07 +0100

    Removed everything for maven repo

--- easycukes-commons/src/main/java/com/worldline/easycukes/commons/stepdefs/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * This package aims at containing all Cucumber stepdefs to be used while dealing with common operations.
- *
- * @author aneveux
- *
- */
-package com.worldline.easycukes.commons.stepdefs;
